a6fe1fdfb2387572fddda6ccf50e0d5845af4eb4
th.obaland@gmail.com <th.obaland@gmail.com>
2024-04-10 21:06:29 +0900

    change: install scripts.

--- install.sh
+++ install.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 
-# Usage: install.sh [type]
-#   <type>
-#     all: Installed all.
-#     vim: Installed related to vim.
-#     shell: Installed related to shell.
-#     app: Installed related to app.
-
 create_link() {
-  if [ -e $2 ]; then
-    rm -f $2
+  if [ -e "$2" ]; then
+    rm -f "$2"
   fi
-  ln -s $1 $2
+  ln -s "$1" "$2"
   echo "create link: $1 -> $2"
 }
-
-TYPE=all
-if [ -n "$1" ]; then
-  if [ $1 = "all" ] || [ $1 = "vim" ] || [ $1 = "shell" ] || [ $1 = "app" ]; then
-    TYPE=$1
-  else
-    echo "Type '$1' is not supported."
-    exit
-  fi
-fi
 
 readonly ROOTDIR=$(cd $(dirname $0) && pwd)
 readonly HOMEDIR=$(cd ~ && pwd)
@@ -32,74 +15,86 @@ echo "Start [Install environment] ..."
 
 # Vim or Neovim
 #-----------------------------------------------------------------------------
-if [ $TYPE = "all" ] || [ $TYPE = "vim" ]; then
-  readonly VIMDIR="$ROOTDIR/vimrc"
-  readonly VIMRC="$VIMDIR/vimrc"
+readonly VIMDIR="$ROOTDIR/vimrc"
+readonly VIMRC="$VIMDIR/vimrc"
 
-  readonly LINKVIMRC="$HOMEDIR/.vimrc"
-  create_link $VIMRC $LINKVIMRC
+readonly LINKVIMRC="$HOMEDIR/.vimrc"
+create_link "$VIMRC" "$LINKVIMRC"
 
-  readonly XDGCONFIGDIR="$HOMEDIR/.config"
-  readonly LINKXDGCONFIG="$XDGCONFIGDIR/nvim"
+readonly XDGCONFIGDIR="$HOMEDIR/.config"
+readonly LINKXDGCONFIG="$XDGCONFIGDIR/nvim"
 
-  if [ ! -d ${XDGCONFIGDIR} ]; then
-    mkdir ${XDGCONFIGDIR}
-  fi
-  create_link $VIMDIR $LINKXDGCONFIG
-
-  readonly LINKVIM="$HOMEDIR/.vim"
-  create_link $VIMDIR $LINKVIM
+if [ ! -d "$XDGCONFIGDIR" ]; then
+  mkdir "$XDGCONFIGDIR"
 fi
+create_link "$VIMDIR" "$LINKXDGCONFIG"
+
+readonly LINKVIM="$HOMEDIR/.vim"
+create_link "$VIMDIR" "$LINKVIM"
 
 # Shell
 #-----------------------------------------------------------------------------
-if [ $TYPE = "all" ] || [ $TYPE = "shell" ]; then
-  readonly OMZDIR="$HOMEDIR/.oh-my-zsh"
-  readonly OMZTHEMEDIR="$OMZDIR/themes"
-  if [ ! -d $OMZTHEMEDIR ]; then
-    echo "Installation of `oh-my-zsh` is required." 1>&2
-    exit
-  fi
-
-  readonly OMZTHEME="$ROOTDIR/configs/obaland.zsh-theme"
-  readonly LINKOMZTHEME="$OMZTHEMEDIR/obaland.zsh-theme"
-  create_link $OMZTHEME $LINKOMZTHEME
-
-  readonly ZSHRC="$ROOTDIR/configs/zshrc"
-  readonly LINKZSHRC="$HOMEDIR/.zshrc"
-  create_link $ZSHRC $LINKZSHRC
-
-  readonly TMUXCONF="$ROOTDIR/configs/tmux.conf"
-  readonly LINKTMUXCONF="$HOMEDIR/.tmux.conf.local"
-  create_link $TMUXCONF $LINKTMUXCONF
-
-  # zsh plugins
-  readonly ZSHPLUGINS="$HOMEDIR/.oh-my-zsh/custom/plugins"
-
-  readonly ZSH_PLUGIN_AUTOSUGGESTIONS_NAME="zsh-autosuggestions"
-  readonly ZSH_PLUGIN_AUTOSUGGESTIONS_PATH="$ZSHPLUGINS/zsh-autosuggestions"
-  if [ ! -d $ZSH_PLUGIN_AUTOSUGGESTIONS_PATH ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_PLUGIN_AUTOSUGGESTIONS_PATH"
-  else
-    echo "'$ZSH_PLUGIN_AUTOSUGGESTIONS_NAME' alreay exists." 1>&2
-  fi
+readonly OMZDIR="$HOMEDIR/.oh-my-zsh"
+readonly OMZTHEMEDIR="$OMZDIR/themes"
+if [ ! -d "$OMZTHEMEDIR" ]; then
+  echo "Installation of `oh-my-zsh` is required." 1>&2
+  exit
 fi
+
+readonly OMZTHEME="$ROOTDIR/configs/obaland.zsh-theme"
+readonly LINKOMZTHEME="$OMZTHEMEDIR/obaland.zsh-theme"
+create_link "$OMZTHEME" "$LINKOMZTHEME"
+
+readonly ZSHRC="$ROOTDIR/configs/zshrc"
+readonly LINKZSHRC="$HOMEDIR/.zshrc"
+create_link "$ZSHRC" "$LINKZSHRC"
+
+# zsh plugin manager
+readonly ZPLUG="$HOMEDIR/.zplug"
+if [ ! -d "$ZPLUG" ]; then
+  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
+
+#readonly ZSHPLUGINS="$HOMEDIR/.oh-my-zsh/custom/plugins"
+#
+#readonly ZSH_PLUGIN_AUTOSUGGESTIONS_NAME="zsh-autosuggestions"
+#readonly ZSH_PLUGIN_AUTOSUGGESTIONS_PATH="$ZSHPLUGINS/zsh-autosuggestions"
+#if [ ! -d "$ZSH_PLUGIN_AUTOSUGGESTIONS_PATH" ]; then
+#  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_PLUGIN_AUTOSUGGESTIONS_PATH"
+#else
+#  echo "'$ZSH_PLUGIN_AUTOSUGGESTIONS_NAME' alreay exists." 1>&2
+#fi
+
+# tmux
+#-----------------------------------------------------------------------------
+readonly TMUX="$HOMEDIR/.tmux"
+if [ ! -d "$TMUX" ]; then
+  git clone https://github.com/gpakosz/.tmux.git "$TMUX"
+  create_link "$TMUX/tmux.conf" "$HOMEDIR/.tmux.conf"
+fi
+
+readonly TMUXTMP="$TMUX/plugins/tpm"
+if [ ! -d "$TMUXTMP" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$TMUXTMP"
+fi
+
+readonly TMUXCONF="$ROOTDIR/configs/tmux.conf"
+readonly LINKTMUXCONF="$HOMEDIR/.tmux.conf.local"
+create_link "$TMUXCONF" "$LINKTMUXCONF"
 
 # Application
 #-----------------------------------------------------------------------------
-if [ $TYPE = "all" ] || [ $TYPE = "app" ]; then
-  # Karabiner for macOS
-  if [ "$(uname)" = "Darwin" ]; then
-    readonly KARABINER="karabiner-for-vim.json"
-    readonly KARABINER_SRCPATH="${ROOTDIR}/configs/${KARABINER}"
-    readonly KARABINER_DESTDIR="${HOMEDIR}/.config/karabiner/assets/complex_modifications"
-    readonly KARABINER_LINKPATH="${KARABINER_DESTDIR}/${KARABINER}"
+# Karabiner for macOS
+if [ "$(uname)" = "Darwin" ]; then
+  readonly KARABINER="karabiner-for-vim.json"
+  readonly KARABINER_SRCPATH="${ROOTDIR}/configs/${KARABINER}"
+  readonly KARABINER_DESTDIR="${HOMEDIR}/.config/karabiner/assets/complex_modifications"
+  readonly KARABINER_LINKPATH="${KARABINER_DESTDIR}/${KARABINER}"
 
-    if [ -d ${KARABINER_DESTDIR} ]; then
-      create_link $KARABINER_SRCPATH $KARABINER_LINKPATH
-    else
-      echo "[ERROR] Require karabiner-elements. (Please install [brew install --cask karabiner-elements])" 1>&2
-    fi
+  if [ -d "$KARABINER_DESTDIR" ]; then
+    create_link "$KARABINER_SRCPATH" "$KARABINER_LINKPATH"
+  else
+    echo "[ERROR] Require karabiner-elements. (Please install [brew install --cask karabiner-elements])" 1>&2
   fi
 fi
 
